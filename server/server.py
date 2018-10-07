ceb89d0ac61a85f2d4eb8c6fb4217edac4276f75
Kawser Nasim <kawsernasim@Kawsers-MacBook-Pro.local>
2018-10-06 19:30:45 -0500

    flask server is running

--- server/server.py
+++ server/server.py
@@ -1,10 +1,22 @@
+import random
 from flask import Flask
+from firebase import firebase
+
 app = Flask(__name__)
 
-@app.route('/')
+firebase = firebase.FirebaseApplication('https://hackspot12.firebaseio.com', None)
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
+    
+    result = firebase.post('/users', {'X_FANCY_HEADER': 'VERY FANCY'})
     return 'hello'
+    
+
+@app.route('/add', methods=['POST'])
+def addUser():
+    return 'add'
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='localhost', port=8081, debug=True)
